8af80d831434b26484e145f6957352bffac0345a
Pedro Henrique Longo <125682292+pedrolong@users.noreply.github.com>
2024-05-03 15:19:35 -0300

    Co-authored-by: caroline176 <caroline176@users.noreply.github.com> Co-authored-by: ana julia  <anaju555@users.noreply.github.com>

--- src/components/imgComponent.js
+++ src/components/imgComponent.js
@@ -3,20 +3,17 @@ import { styles } from "../styles/Style";
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
-export default function Img({ caminhoImg, txt, txtConteudo,nameFunction, nomecidade }) {
-  const [visible, setVisible] = useState(false);
-  visModal = (vis) => {
-    if (!visible) {
-      setVisible(vis);
-    } else {
-      setVisible(vis);
-    }
-  };
-const navigation= useNavigation();
+export default function Img({ caminhoImg, txt, txtConteudo, nameFunction, nomecidade, }) {
+  const navigation = useNavigation();
   return (
     <View style={styles.algconteudo}>
       <Image style={styles.img} source={caminhoImg} />
-      <TouchableOpacity  style={styles.button} onPress={() =>{ visModal(true); navigation.navigate(nameFunction) }}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          navigation.navigate(nameFunction);
+        }}
+      >
         <Text style={styles.txtImg}>{txt}</Text>
       </TouchableOpacity>
       <Text style={styles.txt_introduçaocidade}> É {nomecidade}</Text>
